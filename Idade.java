bc2ca6fa831783561b6156ef0c168c7213afeca5
Jefté Araujo <jefte.banderas@gmail.com>
2018-03-05 13:51:57 -0300

    Update Idade.java

--- Idade.java
+++ Idade.java
@@ -13,10 +13,12 @@ public class Idade {
 		LocalDate date = LocalDate.parse(data,formatter);
 		Period idade = Period.between(date, LocalDate.now());
 		
-		JOptionPane.showMessageDialog(null, date);
-		JOptionPane.showMessageDialog(null, idade);
-		JOptionPane.showMessageDialog(null, idade.getYears());
+		JOptionPane.showMessageDialog(null, "data de nascimento" + date);
+		JOptionPane.showMessageDialog(null, "<< idade completa formato Period >>\n" + idade);
+		
+		JOptionPane.showMessageDialog(null, "idade: " + idade.getYears());
+		JOptionPane.showMessageDialog(null, "Meses: " + idade.getMonths());
+		JOptionPane.showMessageDialog(null, "dias: " + idade.getDays());
 		
 	}
 
-}
